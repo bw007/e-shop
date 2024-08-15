e99a5402cc3738854b0097cf9fc4d9a824685039
bw007 <mr.abdulaziz00791@gmail.com>
2024-08-15 17:01:03 +0500

    Config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,14 @@ module.exports = {
     extend: {
       fontFamily: {
         dewi: ['RF Dewi', 'sans-serif']
-      }
+      },
+      maxWidth: {
+        'screen-sm': '640px',
+        'screen-md': '768px',
+        'screen-lg': '1024px',
+        'screen-xl': '1280px',
+        'screen-2xl': '1536px',
+      },
     },
     container: {
       center: true,
