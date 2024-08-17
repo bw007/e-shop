ec1aaf9349cfef7a397ce800caa749e8eb952746
bw007 <abdulaziz00791@gmail.com>
2024-08-17 18:03:43 +0500

    Slider config

---
+++ src/ts/main.ts
@@ -0,0 +1,30 @@
+var swiper = new Swiper("#slider", {
+  slidesPerView: 1,
+  spaceBetween: 10,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    360: {
+      slidesPerView: 2,
+      spaceBetween: 10,
+    },
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    },
+    1280: {
+      slidesPerView: 4,
+      spaceBetween: 40,
+    },
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  }
+});
